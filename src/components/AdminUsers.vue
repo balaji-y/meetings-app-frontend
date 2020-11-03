c5585e4a4e48104d71b985ad633ccb6239ab508f
Yadakuppam Balaji <yadakuppam.balaji@rupeek.com>
2020-11-03 22:44:22 +0530

    users done

--- src/components/AdminUsers.vue
+++ src/components/AdminUsers.vue
@@ -21,6 +21,85 @@
                     :search="search"
                     class="elevation-1"
                     >
+                     <template v-slot:top>
+                        <v-toolbar
+                        flat
+                        >
+                       
+                       
+                        
+                        <v-dialog
+                            v-model="dialog"
+                            max-width="500px"
+                        >
+                            <v-card>
+                                <v-card-title>
+                                    <span class="headline">Edit User</span>
+                                </v-card-title>
+                
+                            <v-card-text>
+                                <v-container>
+                                <v-row>
+                                    <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="4"
+                                    >
+                                    <v-text-field
+                                        v-model="editedItem._id"
+                                        label="Id"
+                                        :disabled =true
+                                    ></v-text-field>
+                                    </v-col>
+
+                                    <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="4"
+                                    >
+                                    <v-text-field
+                                        v-model="editedItem.name"
+                                        label="Name"
+                                    ></v-text-field>
+                                    </v-col>
+                                    
+                                    
+                                </v-row>
+                                </v-container>
+                            </v-card-text>
+                
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="close"
+                                >
+                                Cancel
+                                </v-btn>
+                                <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="save"
+                                >
+                                Save
+                                </v-btn>
+                            </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                        <v-dialog v-model="dialogDelete" max-width="500px">
+                            <v-card>
+                            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                <v-spacer></v-spacer>
+                            </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                        </v-toolbar>
+                    </template>
                      <template v-slot:item.actions="{ item }">
                         <v-icon
                         small
@@ -47,7 +126,7 @@
 
 <script>
 import AdminNavbar from './AdminNavbar'
-import { getAllUsers } from '../services/admin-users'
+import { getAllUsers,deleteUser, updateUser } from '../services/admin-users'
 export default {
     name:'AdminUsers',
     components:{
@@ -55,10 +134,14 @@ export default {
     },
     data(){
         return {
+            dialog:false,
+            dialogDelete:false,
             editedIndex:-1,
             editedItem:{
                 name:'',
-                email:''
+            },
+            defaultItem:{
+                name:'',
             },
             search:'',
             headers:[
@@ -71,21 +154,64 @@ export default {
         }
     },
     methods:{
-    editItem (item) {
-      this.editedIndex = this.users.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
+        editItem (item) {
+            this.editedIndex = this.users.indexOf(item)
+            //console.log(item);
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
 
-    deleteItem (item) {
-      this.editedIndex = this.userss.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
+        deleteItem (item) {
+            this.editedIndex = this.users.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialogDelete = true
+        },
 
-    deleteItemConfirm () {
-      this.users.splice(this.editedIndex, 1)
-      this.closeDelete()
+        deleteItemConfirm () {
+            this.users.splice(this.editedIndex, 1)
+            //console.log(this.editedItem);
+            deleteUser(this.editedItem._id)
+            .then(response => {
+                console.log(response);
+                console.log('deleted');
+                this.closeDelete()
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+        },
+        close () {
+            this.dialog = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+        save () {
+            if (this.editedIndex > -1) {
+                console.log(this.editedItem)
+                Object.assign(this.users[this.editedIndex], this.editedItem)
+                updateUser(this.editedItem)
+                .then(response => {
+                    console.log(response);
+                    console.log('updated');
+                    this.close();
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            } else {
+                this.users.push(this.editedItem);
+                this.close();
+            }
+            
+        },
+        closeDelete () {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+            this.editedItem = Object.assign({}, this.defaultItem)
+            this.editedIndex = -1
+      })
     },
     },
     mounted(){
--- src/services/admin-users.js
+++ src/services/admin-users.js
@@ -12,3 +12,32 @@ export async function getAllUsers(){
         throw error;
     }
 }
+
+export async function deleteUser(id){
+    const headers = {
+        'Authorization' : localStorage.getItem('token')
+    }
+    try{
+        const response = await axios.delete(`http://localhost:3000/api/users/deleteUser?id=${id}`,{headers:headers});
+        return response.data
+    }
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function updateUser(user){
+    console.log("check",user._id);
+    const headers = {
+        'Authorization' : localStorage.getItem('token')
+    }
+    try{
+        const response = await axios.patch('http://localhost:3000/api/users/updateUser',{user:user},{headers:headers});
+        return response.data
+    }
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+}
